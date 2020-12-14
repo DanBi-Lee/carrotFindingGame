92aa48bbffc1ac437117a79fec5e59d1dd78424d
DanBi-Lee <danbi.s.rain@gmail.com>
2020-12-14 14:21:38 +0900

    [#7] PC가 아닌 환경에서 자동 가로모드

--- script/screen.js
+++ script/screen.js
@@ -1,34 +1,55 @@
 window.addEventListener('load', ()=>{
+    const $body = document.querySelector('body');
     const $wrap = document.querySelector('.wrap');
     let browserWidth = window.innerWidth;
     let browserHeight = window.innerHeight;
     const IMAGE_WIDTH = $wrap.clientWidth;
     const IMAGE_HEIGHT = $wrap.clientHeight;
     const IMAGE_RATE = IMAGE_WIDTH/IMAGE_HEIGHT;
-
+    let isMobile = false;
+    let isPortrait = false;
     
-    function setBackground(){
+    function setBackground(ismobileLandscapeMode){
         browserWidth = window.innerWidth;
         browserHeight = window.innerHeight;
-        
-        if ( browserWidth/browserHeight < IMAGE_RATE ){ 
-            // 이미지 비율보다 높이가 길다
-            $wrap.style.transform = `scale(${browserWidth/IMAGE_WIDTH})`;
-        }else {
-            // 이미지 비율이 같거나 너비가 같다.
-            $wrap.style.transform = `scale(${browserHeight/IMAGE_HEIGHT})`;
+
+        if(ismobileLandscapeMode){
+            const IMAGE_RATE_LANDSCAPE = IMAGE_HEIGHT/IMAGE_WIDTH;
+            if ( browserWidth/browserHeight < IMAGE_RATE_LANDSCAPE ){ 
+                $wrap.style.transform = `scale(${browserWidth/IMAGE_HEIGHT})`;
+            }else {
+                $wrap.style.transform = `scale(${browserHeight/IMAGE_WIDTH})`;
+            }
+        }else{
+            if ( browserWidth/browserHeight < IMAGE_RATE ){ 
+                $wrap.style.transform = `scale(${browserWidth/IMAGE_WIDTH})`;
+            }else {
+                $wrap.style.transform = `scale(${browserHeight/IMAGE_HEIGHT})`;
+            }
         }
     }
     
-    function screenRatio(){
+    function screenRatio(ismobileLandscapeMode){
         window.addEventListener('resize',function (){
-            setBackground();
+            setBackground(ismobileLandscapeMode);
         })
-        setBackground();
+        setBackground(ismobileLandscapeMode);
+    }
+
+    function checkMobileLandscapeMode(){
+        isMobile = /iPhone|iPad|iPod|Android|Mobile/i.test(navigator.userAgent);
+        isPortrait = window.outerWidth < window.outerHeigh;
+        if(isMobile && isPortrait){
+            $body.classList.add('landscape');
+            return true;
+        }else{
+        }
     }
 
     function init(){
-        screenRatio();
+        const ismobileLandscapeMode = checkMobileLandscapeMode()
+        screenRatio(ismobileLandscapeMode);
+
     }
     
     init();
